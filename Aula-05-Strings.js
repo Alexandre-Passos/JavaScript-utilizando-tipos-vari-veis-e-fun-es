aaaff76403f37433fc6ed27ce06206564d874055
Alexandre-Passos <alexandre15passos@gmail.com>
2025-06-13 19:08:56 -0300

    5

---
+++ Aula-05-Strings.js
@@ -0,0 +1,12 @@
+const estudante = 'Alexandre'
+const docente = 'Beatriz'
+const saudacao = 'Estudar é com a gente! HUuhAa! Estudar é bom!'
+const recitacao = 'Estudar é bom'
+
+console.log(saudacao)
+console.log(recitacao)
+
+console.log(`O Estudante se chama '${estudante}'`)
+
+const senha_segura = 'SenhaMuitoSegura' + estudante.toLocaleUpperCase()
+console.log(senha_segura)
