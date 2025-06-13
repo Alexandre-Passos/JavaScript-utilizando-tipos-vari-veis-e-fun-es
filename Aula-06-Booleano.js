f0de97a8ba1ad8f0908d54f1537781a205dfe688
Alexandre-Passos <alexandre15passos@gmail.com>
2025-06-13 19:16:58 -0300

    6

---
+++ Aula-06-Booleano.js
@@ -0,0 +1,14 @@
+const estudante = 'Alexandre'
+const statusAprovacao = true
+
+if (statusAprovacao == true) {
+    console.log('Aluno:', estudante, 'Aprovado')
+} else {
+console.log('REPROVADO')
+}
+
+if (estudante == 'Alexandre'){
+    console.log('Tudo certo')
+}else{
+    console.log('Quem é você?')
+}
