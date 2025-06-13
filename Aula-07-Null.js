e2b853e0deac48a7d3bcae7588643033e63086e1
Alexandre-Passos <alexandre15passos@gmail.com>
2025-06-13 19:30:45 -0300

    Null-e-Undefined

---
+++ Aula-07-Null.js
@@ -0,0 +1,12 @@
+let vazio;
+let nomeVazio = ''
+let nomeEstudante = 'Alexandre'
+console.log(typeof vazio)
+console.log(typeof nomeVazio)
+console.log(typeof nomeEstudante)
+
+let telefone = null
+console.log(typeof telefone)
+console.log(telefone + 5)
+console.log(vazio + 5)
+console.log(telefone + vazio)
