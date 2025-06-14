b8a059fc1ba6e5fe86a92a27f31b9e542f4bcf09
Alexandre-Passos <alexandre15passos@gmail.com>
2025-06-14 06:19:23 -0300

    11

---
+++ Aula-11-Operadores-Comparacao.js
@@ -0,0 +1,30 @@
+const statusAprovacao = true
+
+if (statusAprovacao === true) {
+    console.log('Aprovado')
+} else {
+    console.log('Reprovado')
+}
+
+if ('0' == 0) {
+    console.log('Verdadeiro')
+} else {
+    console.log('Falso')
+
+}
+
+if ('0' === 0) {
+    console.log('Verdadeiro, de mesmo tipo')
+} else {
+    console.log('Falso. São de tipos diferentes')
+
+}
+
+console.log('\n')
+
+instituicao = 'UEA'
+if (instituicao !== 'Aluno') {
+    console.log('Verdadeiro')
+} else {
+    console.log('Falso')
+}
